296ebfa57076e1a11bc146f91409e0ab26b39ec4
srikanth jeldi <srikanth.jeldi@gmail.com>
2026-07-26 19:41:25 +0530

    Remove unsupported seamless rotation scaling

--- mobile/android/app/src/main/kotlin/com/epitomehub/chessverse/MainActivity.kt
+++ mobile/android/app/src/main/kotlin/com/epitomehub/chessverse/MainActivity.kt
@@ -1,14 +1,5 @@
 package com.epitomehub.chessverse
 
-import android.os.Bundle
-import android.view.WindowManager
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        window.attributes = window.attributes.apply {
-            rotationAnimation = WindowManager.LayoutParams.ROTATION_ANIMATION_SEAMLESS
-        }
-    }
-}
+class MainActivity : FlutterActivity()
