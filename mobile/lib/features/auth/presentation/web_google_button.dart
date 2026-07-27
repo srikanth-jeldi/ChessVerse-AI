00bdf7c0cb019bdf80d7fc425128f88e8e3a8ac1
srikanth jeldi <srikanth.jeldi@gmail.com>
2026-07-27 23:21:51 +0530

    Isolate web Google sign-in UI

---
+++ mobile/lib/features/auth/presentation/web_google_button.dart
@@ -0,0 +1,2 @@
+export 'web_google_button_stub.dart'
+    if (dart.library.js_interop) 'web_google_button_web.dart';
---
+++ mobile/lib/features/auth/presentation/web_google_button_stub.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/widgets.dart';
+
+Widget buildWebGoogleSignInButton() => const SizedBox.shrink();
